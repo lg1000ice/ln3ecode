b22cfd8ab2d2767f25305faf2ad61e46348a8859
lg1000ice <142186904+lg1000ice@users.noreply.github.com>
2023-08-15 10:27:33 +0800

    Update DEATHNOTE (rickroll).cpp

--- DEATHNOTE (rickroll).cpp
+++ DEATHNOTE (rickroll).cpp
@@ -31,7 +31,7 @@ int main()
 		return 0;
 	}
 	cout<<"You are rickrolled!";
-	system("start https://www.bilibili.com/video/BV1GJ411x7h7/?spm_id_from=333.337.search-card.all.click&vd_source=375d7bd9134404b2b1ade28b09a56067");
+	system("start https://www.bilibili.com/video/BV1GJ411x7h7/?spm_id_from=333.337.search-card.all.click");
 	return 0;
 }
  
