06f1746adf7b63940da02dd5f98db6449cef9d53
lg1000ice <142186904+lg1000ice@users.noreply.github.com>
2023-08-17 17:06:57 +0800

    Create 当我拿出此代码，阁下又该如何应对（define乱用）.cpp

---
+++ 当我拿出此代码，阁下又该如何应对（define乱用）.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#define homo int
+#define happy return
+#define oiiai >=
+#define oooooiai <=
+#define yiyisiwuyisi if
+#define zhaoqi char
+#define zhaocha while
+#define huaqiang &&
+#define rickroll +=
+#define you 1
+#define giveyou cin
+#define letyou cout
+#define up >>
+#define down <<
+#define cat 0 
+using namespace std;
+zhaoqi a;
+homo sum1,sum2,sum3;
+homo main()
+{
+	zhaocha(giveyou up a)
+	{
+		yiyisiwuyisi(a oiiai 'A' huaqiang a oooooiai 'Z')
+		{
+			sum1 rickroll you;
+		}
+		yiyisiwuyisi(a oiiai 'a' huaqiang a oooooiai 'z')
+		{
+			sum2 rickroll you;
+		}
+		yiyisiwuyisi(a oiiai '0' huaqiang a oooooiai '9')
+		{
+			sum3 rickroll you;
+		}
+	}
+	letyou down sum1 down " " down sum2 down " " down sum3;
+	happy cat;
+}
